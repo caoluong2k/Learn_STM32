95945078c9cd7b59a53ead0ad457850e068f2e43
caoluong2k <92367249+caoluong2k@users.noreply.github.com>
2022-08-16 22:14:17 +0700

    Add files via upload

---
+++ Soft_UART_Lite/Soft_UART_Lite_Inc/Soft_UART_Lite.h
@@ -0,0 +1,122 @@
+/**
+  ***********************************************************************
+  * @file    	Soft_UART.h                                             *
+  * @author 	BIG SIX                                                 *
+  * @created 	May 29, 2022                                            *
+  * @brief   	This file contains all the function prototypes for      *
+  *          	the Soft_UART.c file                                    *
+  ***********************************************************************
+*/
+
+#ifndef __Soft_UART_H__
+#define __Soft_UART_H__
+
+/******************* Include Library ************************/
+#include "main.h"
+#include "string.h"
+/****************** Define TIMER 3 *********************/
+extern	TIM_HandleTypeDef htim3;
+#define TIMER_COUNT					49
+#define Timer_Frequency				72000000
+
+/************* Define Parameter for UART  ************/
+#define Number_Of_SoftUarts 			2
+#define Soft_Uart_Tx_Buffer_Size		32
+#define Soft_Uart_Rx_Buffer_Size		64
+
+#define Soft_Uart_BAUD_RATE				9600
+#define Soft_Uart_DATA_LEN				7
+#define Soft_Uart_STOP_BIT				1
+#define Soft_Uart_PARITY_BIT_MODE		0  // 0: None Parity, 1: Even Parity, 2: Odd Parity
+
+#define Soft_Uart_PORT_TX_1				GPIOA
+#define Soft_Uart_PIN_TX_1				GPIO_PIN_5
+
+#define Soft_Uart_PORT_TX_2				GPIOA
+#define Soft_Uart_PIN_TX_2				GPIO_PIN_4
+
+#define SoftUart_DATA_LEN_C1 			(Soft_Uart_DATA_LEN + 1)
+#define SoftUart_DATA_LEN_C2 			(Soft_Uart_DATA_LEN + 2)
+
+/*
+ * @brief State of UART
+ */
+typedef enum {
+	SoftUart_OK,
+	SoftUart_Error
+}SoftUartState_E;
+
+/*
+ * @brief  RX and TX Buffer Size for UART
+ */
+typedef struct{
+	uint8_t					Tx[Soft_Uart_Tx_Buffer_Size];
+	uint8_t					Rx[Soft_Uart_Rx_Buffer_Size];
+}SoftUartBuffer_S;
+
+///*
+// * @brief Set parity bit
+// */
+//typedef enum{
+//	None_Prity_Bit,
+//	Even_Parity_Bit,
+//	Odd_Parity_Bit
+//}SoftUartParityBit;
+
+/*
+ * @brief 	Parameter for UART: Pin, Port TX and RX, Data Buffer, ...
+ */
+typedef struct {
+	__IO uint8_t			TxNComplated;
+	uint8_t					TxEnable;
+	uint8_t					TxBitShift, TxBitCounter;
+	uint8_t					TxIndex, TxSize;
+	SoftUartBuffer_S		*Buffer;
+	GPIO_TypeDef			*TxPort;
+	uint16_t				TxPin;
+} SoftUart_S;
+
+/************************ Define UART Function ***************************/
+/*
+ * @brief	Calculate Baudrate
+ * @param	baudrate of UART
+ * @return	Prescaler (PSC)
+ * @note	In this project, Timer3 Frequency value is 72MHz and Timer Counter value is 49.
+ * 			However, with baud = 11500, the transmitted data may not be correct
+ * 			(if TIMER_COUNT = 50 and PSC = 11 then transmitted correct)
+ */
+uint16_t Soft_Uart_Set_Baudrate(uint16_t baudrate);
+
+/*
+ * @brief	Init Port, Pin, Count variable and Buffer for UART
+ * @param	Soft UART Number
+ * 			Ports to Transmit and Receive Data
+ * 			Pins to Transmit and Receive Data
+ * @return	State of UART (init OK or Error)
+ */
+SoftUartState_E		Soft_Uart_Init(uint8_t SoftUartNumber, GPIO_TypeDef *TxPort, uint16_t TxPin);
+
+/*
+ * @brief 	Handle UART.
+ * @note	This function must call in interrupt every 1/Baudrate (second)
+ */
+void 				Soft_Uart_Handler(void);
+//void				Soft_Uart_Handler_1(void);
+
+/*
+ * @brief 	Copy Data to Transmit Buffer and Start Sending
+ * @param 	Soft UART Number
+ * 			Data you want to transmit
+ * 			Length of Data
+ * @return	State of UART (OK or Error)
+ */
+SoftUartState_E 	Soft_Uart_Puts(uint8_t SoftUartNumber, char *str, uint8_t len);
+
+/*
+ * @brief 	Soft Uart Transmit Data Process
+ * @param 	Soft UART
+ * @note	this function is used in Soft_Uart_Handler
+ */
+void Soft_Uart_TX_Process (SoftUart_S *SU);
+
+#endif /* __Soft_UART_H__ */
---
+++ Soft_UART_Lite/Soft_UART_Lite_Src/Soft_UART_Lite.c
@@ -0,0 +1,167 @@
+/**
+ * ******************************************************************************
+ * @file 		Soft_UART.c
+ * @auhtor		BIG SIX
+ * @created		May 29, 2022
+ * @brief		Soft UART Library for STM32F103C8T6
+ * 				This file provides code for the configuration of the UART instances
+ * *******************************************************************************
+ */
+
+/* Include Library */
+#include "Soft_UART_Lite.h"
+
+/* All Soft Uart Config and State */
+SoftUart_S				SUart[Number_Of_SoftUarts];
+
+/* TX RX Data Buffer */
+SoftUartBuffer_S 		SUartBuffer[Number_Of_SoftUarts];
+
+/* Read RX single Pin Value */
+GPIO_PinState Soft_Uart_GPIO_Read_Pin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin){
+	return HAL_GPIO_ReadPin(GPIOx, GPIO_Pin);
+}
+
+/* Calculate Baudrate */
+
+uint16_t Soft_Uart_Set_Baudrate(uint16_t baudrate){
+	uint16_t PSC_Value;
+	if (baudrate == 115200) {
+		return PSC_Value = 11;
+	}
+	else {
+		PSC_Value = (Timer_Frequency/(baudrate*(TIMER_COUNT))) - 1;
+		return PSC_Value;
+	}
+}
+
+/* Init Soft UART */
+SoftUartState_E Soft_Uart_Init(uint8_t SoftUartNumber, GPIO_TypeDef *TxPort, uint16_t TxPin){
+	if (SoftUartNumber >= Number_Of_SoftUarts) {
+		return SoftUart_Error;
+	}
+
+	SUart[SoftUartNumber].TxNComplated = 0;
+
+	SUart[SoftUartNumber].TxEnable = 0;
+
+	SUart[SoftUartNumber].TxBitCounter = 0;
+	SUart[SoftUartNumber].TxBitShift = 0;
+	SUart[SoftUartNumber].TxIndex = 0;
+
+	SUart[SoftUartNumber].TxSize = 0;
+
+	SUart[SoftUartNumber].Buffer = &SUartBuffer[SoftUartNumber];
+
+	SUart[SoftUartNumber].TxPort = TxPort;
+	SUart[SoftUartNumber].TxPin = TxPin;
+
+	return SoftUart_OK;
+}
+
+/* Write TX single Pin Value */
+void Soft_Uart_GPIO_Write_Pin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState){
+	HAL_GPIO_WritePin(GPIOx, GPIO_Pin, PinState);
+}
+
+/* Send one bit to TX pin */
+void Soft_Uart_Transmit_Bit(SoftUart_S *SU, uint8_t Bit0_1){
+	Soft_Uart_GPIO_Write_Pin(SU->TxPort, SU->TxPin, (GPIO_PinState)Bit0_1);
+}
+
+/* Soft Uart Transmit Data Process */
+void Soft_Uart_TX_Process(SoftUart_S *SU){
+	if (SU->TxEnable) {
+		// Start Bit
+		if (SU->TxBitCounter == 0) {
+			SU->TxNComplated = 1;
+			SU->TxBitShift = 0;
+			Soft_Uart_Transmit_Bit(SU, 0);
+			SU->TxBitCounter++;
+		}
+
+		// Data
+		else if (SU->TxBitCounter < SoftUart_DATA_LEN_C1) {
+			if ((((SU->Buffer->Tx[SU->TxIndex]) >> (SU->TxBitShift)) & 0x01) == 0x1) {
+				Soft_Uart_Transmit_Bit(SU, 1);
+			}
+			else {
+				Soft_Uart_Transmit_Bit(SU, 0);
+			}
+			//Soft_Uart_Transmit_Bit(SU, ((SU->Buffer->Tx[SU->TxIndex]) >> (SU->TxBitShift)) & 0x01);
+			SU->TxBitCounter++;
+			SU->TxBitShift++;
+		}
+
+		// Stop Bit
+		else if (SU->TxBitCounter == SoftUart_DATA_LEN_C1) {
+			if (Soft_Uart_STOP_BIT == 1) {
+				Soft_Uart_Transmit_Bit(SUart, 1);
+			}
+			else {
+				Soft_Uart_Transmit_Bit(SUart, 1);
+				Soft_Uart_Transmit_Bit(SUart, 1);
+			}
+			SU->TxBitCounter++;
+		}
+
+		// Complete
+		else if (SU->TxBitCounter == SoftUart_DATA_LEN_C2) {
+			// Reset Bit counter
+			SU->TxBitCounter = 0;
+
+			// ready to send another data
+			SU->TxIndex++;
+
+			// Check Size of Data
+			if (SU->TxSize > SU->TxIndex) {  //TxSize = len of Data
+				// Continue Sending Data
+				SU->TxNComplated = 1;
+				SU->TxEnable = 1;
+			}else {
+				//Finish
+				SU->TxNComplated = 0;
+				SU->TxEnable = 0;
+			}
+		}
+	}
+}
+
+/* Copy Data to Transmit Buffer and Start Sending */
+SoftUartState_E Soft_Uart_Puts(uint8_t SoftUartNumber, char *Str, uint8_t len){
+	int i;
+	if (SoftUartNumber >= Number_Of_SoftUarts) {
+		return SoftUart_Error;
+	}
+
+	if (SUart[SoftUartNumber].TxNComplated) {
+		return SoftUart_Error;
+	}
+
+	SUart[SoftUartNumber].TxIndex = 0;
+	SUart[SoftUartNumber].TxSize = len;
+
+	for (i = 0; i < len; i++) {
+		SUart[SoftUartNumber].Buffer->Tx[i] = Str[i];
+	}
+
+	SUart[SoftUartNumber].TxNComplated = 1;
+	SUart[SoftUartNumber].TxEnable = 1;
+	return SoftUart_OK;
+}
+
+/* Handle UART */
+void Soft_Uart_Handler(void){
+	for (int i = 0; i < Number_Of_SoftUarts; i++) {
+		if (SUart[i].TxEnable) {
+			Soft_Uart_TX_Process(&SUart[i]);
+		}
+	}
+}
+
+
+
+
+
+
+
